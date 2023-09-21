cda0c7927b90ca7d657a78b67c7852a41a65c753
Adolf <adolfnyerere@gmail.com>
2023-09-21 05:26:41 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun (first = "roller-skate"){
+    return(`This Saturday, I want to ${first}!`);
+};
+saturdayFun("bathe my dog");
+function mondayWork (office = "go to the office"){
+    return(`This Monday, I will ${office}.`);
+}
+mondayWork ("work from home");
+function wrapAdjective(noIdea) { 
+    return (function innerF (special = "*") {
+        return `You are ${noIdea}${special}${noIdea}!`;
+    });
+};
+wrapAdjective ("||")(`||||!`);
